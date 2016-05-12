144b1a5abe5db9cf81197951beb70820f5410083
Mark Howard <bark4mark@gmail.com>
2016-05-12 22:37:39 +0100

    Ignoring test for now

--- src/test/java/co/markhoward/uricrawler/downloader/PhantomDownloaderTest.java
+++ src/test/java/co/markhoward/uricrawler/downloader/PhantomDownloaderTest.java
@@ -1,10 +1,12 @@
 package co.markhoward.uricrawler.downloader;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PhantomDownloaderTest {
 	
 	@Test
+	@Ignore
 	public void test() {
 		PhantomDownloader phantomDownloader = new PhantomDownloader();
 		phantomDownloader.download();
